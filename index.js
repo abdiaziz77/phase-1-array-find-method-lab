bf170fcbc9c94765d85ba2cea30f64a4e1ad9a78
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-03-13 16:46:53 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+const broncosRecord = [
+    { year: "2015", team: "Denver Broncos", result: "W" },
+    { year: "2013", team: "Denver Broncos", result: "L" },
+    { year: "1998", team: "Denver Broncos", result: "W" },
+    { year: "1997", team: "Denver Broncos", result: "W" }
+];
+
+console.log(superbowlWin(broncosRecord));
